005430e74e05b9030254bbe8de44ef51cb15954b
AntonMelekhavets <antohage@gmail.com>
2017-12-22 14:02:25 +0300

    Added sending to email

---
+++ EmailManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Mail;
+
+namespace GlobalHooks
+{
+    internal class EmailManager
+    {
+        private readonly SmtpClient _smtpClient;
+        private const string Sender = "melehovec.anton@mail.ru";
+        private const string Password = "fghtykv";
+
+        private const string Host = "smtp.mail.ru";
+        private const int Port = 587;
+
+        public EmailManager()
+        {
+            _smtpClient = new SmtpClient(Host, Port)
+            {
+                Credentials = new System.Net.NetworkCredential(Sender, Password),
+                EnableSsl = true
+            };
+        }
+
+        public void SendEmail(string receiver, string topic, string filePath)
+        {
+            var mail = new MailMessage(Sender, receiver, topic, string.Empty);
+            using (var  attachment = new Attachment(filePath))
+            {
+                mail.Attachments.Add(attachment);
+                _smtpClient.Send(mail);
+            }
+        }
+    }
+}
